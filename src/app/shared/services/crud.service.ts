c45cb5987d27ac763f945a80eb27c727136e3d8a
Isah Ohieku <isahohieku@gmail.com>
2020-04-09 17:59:16 +0100

    set up basic crud methods in crud service file

--- src/app/shared/services/crud.service.ts
+++ src/app/shared/services/crud.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+import { share } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  private header: { headers: HttpHeaders; };
+
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.header = {
+      headers: new HttpHeaders()
+    };
+  }
+
+  getResource(url): Observable<any> {
+    return this.http.get(`${url}`, this.header).pipe(share());
+  }
+
+  postResource(url, data): Observable<any> {
+    return this.http.post(`${url}`, data, this.header).pipe(share());
+  }
+
+  updateResource(url, data): Observable<any> {
+    return this.http.put(`${url}`, data, this.header).pipe(share());
+  }
+
+  deleteResource(url): Observable<any> {
+    return this.http.delete(`${url}`, this.header).pipe(share());
+  }
 }
