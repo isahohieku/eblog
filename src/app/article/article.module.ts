a78e89169146538f5ea88834fdb875e50e5fc384
Isah Ohieku <isahohieku@gmail.com>
2020-04-10 19:20:52 +0100

    added article view component

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditorComponent } from './editor/editor.component';
+import { ViewComponent } from './view/view.component';
 
 
 
 @NgModule({
-  declarations: [EditorComponent],
+  declarations: [EditorComponent, ViewComponent],
   imports: [
     CommonModule
   ]
